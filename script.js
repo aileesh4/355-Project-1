0d5ff2b22295f24daf101d46510ce5c7bbbe14e2
aileesh4 <aileeshjavier@gmail.com>
2023-02-28 20:22:13 -0500

    added hidden button on home page

--- script.js
+++ script.js
@@ -37,3 +37,13 @@ function removeActiveClasses() {
         panel.classList.remove('active')
     })
 }
+
+//hidden search
+const search = document.querySelector('.search')
+const btn = document.querySelector('.btn')
+const input = document.querySelector('.input')
+
+btn.addEventListener('click', () => {
+    search.classList.toggle('active')
+    input.focus()
+})
